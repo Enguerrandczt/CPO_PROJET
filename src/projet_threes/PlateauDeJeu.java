1ca602c136dd11c179b1d3bb95394c2f52ba6927
Lucas <lucas.leonardi@epfedu.fr>
2023-12-20 23:47:49 +0100

    Méthodes pour les 4 mouvements terminées + Méthode qui génére un 1 et 2 au début de la partie + Méthode qui génére 1 2 ou 3 à l'opposé du mouvement.

--- src/projet_threes/PlateauDeJeu.java
+++ src/projet_threes/PlateauDeJeu.java
@@ -32,43 +32,36 @@ public class PlateauDeJeu {
             }
         }
     }
-    
-    public void Générer_Un_Deux() {
-        Random nbAlea = new Random();
-        Random random = new Random();
-        int randomNumber = random.nextInt(2); // Donne soit 0 soit 1
-        int result = (randomNumber == 0) ? 0 : 3;
 
-        if (matriceTuiles[nbAlea.nextInt(4)][result].getValue() == 0) {
-            matriceTuiles[nbAlea.nextInt(4)][result].activerTuile();
-        }
-        if (matriceTuiles[result][nbAlea.nextInt(4)].getValue() == 0) {
-            matriceTuiles[result][nbAlea.nextInt(4)].activerTuile();
-        }
-    }
-    public void Générer_Un_Deux_Trois(String direction) {
+
+public void Générer_Un_Deux_Trois(int Ligne, int Colonne, int value) {
+
+    matriceTuiles[Ligne][Colonne].setValue(value);
+}
+    
+    public void Générer_Opposé_Mouvement(String direction) {
     Random random = new Random();
     int value = random.nextInt(3) + 1; // Génère aléatoirement 1, 2 ou 3
 
     switch (direction) {
-        case "haut":
-            if (matriceTuiles[0][random.nextInt(TaillePlateau)].getValue() == 0) {
-                matriceTuiles[0][random.nextInt(TaillePlateau)].setValue(value);
-            }
-            break;
         case "bas":
-            if (matriceTuiles[TaillePlateau - 1][random.nextInt(TaillePlateau)].getValue() == 0) {
-                matriceTuiles[TaillePlateau - 1][random.nextInt(TaillePlateau)].setValue(value);
+            if (matriceTuiles[0][random.nextInt(TaillePlateau)].getValue() == 0) {
+                matriceTuiles[0][random.nextInt(TaillePlateau)].setValue(value);    
             }
             break;
-        case "gauche":
+        case "haut":
+            if (matriceTuiles[TaillePlateau-1][random.nextInt(TaillePlateau)].getValue() == 0) {
+                matriceTuiles[TaillePlateau-1][random.nextInt(TaillePlateau)].setValue(value);
+            }
+            break;
+        case "droite":
             if (matriceTuiles[random.nextInt(TaillePlateau)][0].getValue() == 0) {
                 matriceTuiles[random.nextInt(TaillePlateau)][0].setValue(value);
             }
             break;
-        case "droite":
-            if (matriceTuiles[random.nextInt(TaillePlateau)][TaillePlateau - 1].getValue() == 0) {
-                matriceTuiles[random.nextInt(TaillePlateau)][TaillePlateau - 1].setValue(value);
+        case "gauche":
+            if (matriceTuiles[random.nextInt(TaillePlateau)][TaillePlateau-1].getValue() == 0) {
+                matriceTuiles[random.nextInt(TaillePlateau)][TaillePlateau-1].setValue(value);
             }
             break;
         default:
@@ -77,7 +70,6 @@ public class PlateauDeJeu {
     }
 }
 
-   
     public void décaler_Ligne_Bas() {
     for (int y = 0; y < TaillePlateau; y++) {
         for (int x = TaillePlateau - 1; x > 0; x--) {
@@ -88,18 +80,106 @@ public class PlateauDeJeu {
                     matriceTuiles[x - 1][y].setValue(0);
                 }
             } else {
-                // Si la case contient une tuile, essayer de fusionner avec la case supérieure
-                if (matriceTuiles[x - 1][y].getValue() == matriceTuiles[x][y].getValue()) {
+                
+                if (matriceTuiles[x - 1][y].getValue() == matriceTuiles[x][y].getValue() && matriceTuiles[x - 1][y].getValue() !=2 && matriceTuiles[x][y].getValue() !=2 && matriceTuiles[x - 1][y].getValue() !=1 && matriceTuiles[x - 1][y].getValue() !=1) {
                     matriceTuiles[x][y].setValue(2 * matriceTuiles[x - 1][y].getValue());
                     matriceTuiles[x - 1][y].setValue(0);
+                    
+                } else { 
+                    
+                if ( (matriceTuiles[x - 1][y].getValue() == 1 && matriceTuiles[x][y].getValue() == 2) || (matriceTuiles[x - 1][y].getValue() == 2 && matriceTuiles[x][y].getValue() == 1) ) {
+                    matriceTuiles[x][y].setValue(3);
+                    matriceTuiles[x - 1][y].setValue(0);
+     
+            }
+        }
+    }
+    }
+    }
+    }
+
+    public void décaler_Ligne_Haut() {
+    for (int y = 0; y < TaillePlateau; y++) {
+        for (int x = 0; x<TaillePlateau-1; x++) {
+            if (matriceTuiles[x][y].getValue() == 0) {
+                // Si la case est vide, déplacer la case supérieure vers le bas
+                if (matriceTuiles[x+1][y].getValue() != 0) {
+                    matriceTuiles[x][y].setValue(matriceTuiles[x+1][y].getValue());
+                    matriceTuiles[x+1][y].setValue(0);
+                }
+            } else {
+                // Si la case contient une tuile, essayer de fusionner avec la case supérieure
+                if (matriceTuiles[x+1][y].getValue() == matriceTuiles[x][y].getValue() && matriceTuiles[x + 1][y].getValue() !=2 && matriceTuiles[x][y].getValue() !=2 && matriceTuiles[x + 1][y].getValue() !=1 && matriceTuiles[x + 1][y].getValue() !=1) {
+                    matriceTuiles[x][y].setValue(2 * matriceTuiles[x+1][y].getValue());
+                    matriceTuiles[x+1][y].setValue(0);
+                    
+                } else {
+                    
+                 if (matriceTuiles[x+1][y].getValue() == 1 & matriceTuiles[x][y].getValue() == 2 || matriceTuiles[x+1][y].getValue() == 2 & matriceTuiles[x][y].getValue() == 1 ) {
+                    matriceTuiles[x][y].setValue(3);
+                    matriceTuiles[x+1][y].setValue(0);
                 }
             }
         }
     }
-    Générer_Un_Deux_Trois("haut");
 }
-
+    }
     
+
+    public void décaler_Colonne_Droite() {
+    for (int y = 0; y < TaillePlateau; y++) {
+        for (int x = TaillePlateau-1; x > 0 ; x--) {
+            if (matriceTuiles[y][x].getValue() == 0) {
+                // Si la case est vide, déplacer la case supérieure vers le bas
+                if (matriceTuiles[y][x-1].getValue() != 0) {
+                    matriceTuiles[y][x].setValue(matriceTuiles[y][x-1].getValue());
+                    matriceTuiles[y][x-1].setValue(0);
+                }
+            } else {
+                // Si la case contient une tuile, essayer de fusionner avec la case supérieure
+                if (matriceTuiles[y][x-1].getValue() == matriceTuiles[y][x].getValue() && matriceTuiles[y][x-1].getValue() !=2 && matriceTuiles[y][x].getValue() !=2 && matriceTuiles[y][x-1].getValue() !=1 && matriceTuiles[y][x+1].getValue() !=1) {
+                    matriceTuiles[y][x].setValue(2 * matriceTuiles[y][x-1].getValue());
+                    matriceTuiles[y][x-1].setValue(0);
+                    
+                } else {
+                    
+                if (matriceTuiles[y][x-1].getValue() == 1 & matriceTuiles[y][x].getValue() == 2 || matriceTuiles[y][x-1].getValue() == 2 & matriceTuiles[y][x].getValue() == 1 ) {
+                    matriceTuiles[y][x].setValue(3);
+                    matriceTuiles[y][x-1].setValue(0);
+                }
+            }
+        }
+    }
+}
+    }
+
+    public void décaler_Colonne_Gauche() {
+    for (int y = 0; y < TaillePlateau; y++) {
+        for (int x = 0; x<TaillePlateau-1 ; x++) {
+            if (matriceTuiles[y][x].getValue() == 0) {
+                // Si la case est vide, déplacer la case supérieure vers le bas
+                if (matriceTuiles[y][x+1].getValue() != 0) {
+                    matriceTuiles[y][x].setValue(matriceTuiles[y][x+1].getValue());
+                    matriceTuiles[y][x+1].setValue(0);
+                }
+            } else {
+                // Si la case contient une tuile, essayer de fusionner avec la case supérieure
+                if (matriceTuiles[y][x+1].getValue() == matriceTuiles[y][x].getValue() && matriceTuiles[y][x+1].getValue() !=2 && matriceTuiles[y][x].getValue() !=2 && matriceTuiles[y][x+1].getValue() !=1 && matriceTuiles[y][x+1].getValue() !=1) {
+                    matriceTuiles[y][x].setValue(2 * matriceTuiles[y][x+1].getValue());
+                    matriceTuiles[y][x+1].setValue(0);
+                    
+                } else {
+                    
+                if (matriceTuiles[y][x+1].getValue() == 1 & matriceTuiles[y][x].getValue() == 2 || matriceTuiles[y][x+1].getValue() == 2 & matriceTuiles[y][x].getValue() == 1 ) {
+                    matriceTuiles[y][x].setValue(3);
+                    matriceTuiles[y][x+1].setValue(0);
+                }
+            }
+        }
+    }
+}
+    }
+
 @Override
 public String toString() {
     String PlateauDeJeu = "";
