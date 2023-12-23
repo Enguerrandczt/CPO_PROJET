7fd8f9fd21c7b76be82767462f0f96f3b6f786ba
Evan ROGIER <evan.rogier@epfedu.fr>
2023-12-23 17:12:50 +0100

    PROJET THREES: Javadoc finie

--- src/projet_threes/Fenêtre_de_Jeu.java
+++ src/projet_threes/Fenêtre_de_Jeu.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+Fenêtre de jeu est utilisée pour créer une fenêtre de jeu pour Threes
  */
 package projet_threes;
 
